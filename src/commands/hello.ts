abb905234d4b2ffcc75dea17a821aa5c3cb854c1
Louis Quinnell <_@ziorobo.co>
2021-09-12 15:30:53 +1000

    Mock the filesystem in tests

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,10 +1,19 @@
 import { Command, Option } from "clipanion"
 import { Context } from "../types"
+import { readdir } from "fs/promises"
 
 export class HelloCommand extends Command<Context> {
 	name = Option.String()
 
 	async execute() {
-		this.context.stdout.write(`Hello ${this.name} from ${this.cli.binaryName}!`)
+		this.context.stdout.write(
+			[
+				`Hello ${this.name} from ${this.cli.binaryName}!`,
+				`I found some files:\n`,
+			].join("\n")
+		)
+
+		const files = await readdir(process.cwd())
+		this.context.stdout.write(files.join("\n"))
 	}
 }
--- test/cli.spec.ts
+++ test/cli.spec.ts
@@ -1,9 +1,19 @@
-import { BaseContext, Cli } from "clipanion"
+import { Cli } from "clipanion"
+import { vol } from "memfs"
 
 import { Context } from "../src/types"
 import { HelloCommand } from "../src/commands/hello"
 
+jest.mock("fs/promises", () => require("memfs").fs.promises)
+
 it(`works`, async () => {
+	vol.fromJSON(
+		{
+			"README.md": "# Hello world!",
+		},
+		process.cwd()
+	)
+
 	const context = {
 		stdout: { write: jest.fn() },
 		stderr: { write: jest.fn() },
@@ -13,9 +23,17 @@ it(`works`, async () => {
 	cli.register(HelloCommand)
 
 	// @ts-ignore
-	await cli.run(["blep"], context as BaseContext)
+	await cli.run(["blep"], context as Context)
 
 	expect(context.stdout.write).toHaveBeenCalledWith(
 		expect.stringMatching("Hello blep from test-cli!")
+	)
+
+	expect(context.stdout.write).toHaveBeenCalledWith(
+		expect.stringMatching("README.md")
+	)
+
+	expect(context.stdout.write).not.toHaveBeenCalledWith(
+		expect.stringMatching("LICENSE")
 	)
 })
